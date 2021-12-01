d8bb0d79d2dce2f35b5e02b0374533bd75a367a3
daniel_serrano_talento <daniel.serrano@talentomobile.com>
2021-12-01 18:23:01 +0100

    [ADD] domain tests structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
--- src/domain/task/task.test.ts
+++ src/domain/task/task.test.ts
@@ -1,1 +1,7 @@
+// import { taskMock1, taskMock2, taskMock3, taskMock4 } from "./task.mock";
+
+// describe("Task entity module", () => {
+//   it
+// })
+
 export {}
