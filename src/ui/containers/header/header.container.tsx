b897ffcc3eed84e54aec41fc14d7f59ae86bec92
daniel_serrano_talento <daniel.serrano@talentomobile.com>
2021-12-02 17:36:39 +0100

    [ADD] header

--- src/ui/containers/header/header.container.tsx
+++ src/ui/containers/header/header.container.tsx
@@ -1,9 +1,41 @@
+import jiraLogo from "ui/assets/images/logo.png";
+import { ReactComponent as SettingsIcon } from "ui/assets/icons/settings.svg";
+import { ReactComponent as QuestionIcon } from "ui/assets/icons/question.svg";
 import styles from "./header.module.scss";
 
 export const Header = (): JSX.Element => {
   return (
     <header className={styles.container}>
-      Jira Clone
+      <section>
+        <div className={styles.logo}>
+          <img src={jiraLogo} width={24} height={24} />
+          <span className={styles.app_title}>Jira Clone</span>
+        </div>
+      </section>
+      <section className={styles.icons_container}>
+        <HeaderIcon>
+          <QuestionIcon />
+        </HeaderIcon>
+        <HeaderIcon>
+          <SettingsIcon />
+        </HeaderIcon>
+      </section>
     </header>
   )
+}
+
+const HeaderIcon = ({ onClick, children }: HeaderIconProps): JSX.Element => {
+  return (
+    <button
+      className={styles.icon_button}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
+interface HeaderIconProps {
+  onClick?: () => void;
+  children: JSX.Element;
 }
