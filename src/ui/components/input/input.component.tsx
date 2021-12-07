55cb11d7def35f465b870246a983208254f852f4
daniel_serrano_talento <daniel.serrano@talentomobile.com>
2021-12-07 16:24:18 +0100

    [FIX] clear button event firing after blur

--- src/ui/components/input/input.component.tsx
+++ src/ui/components/input/input.component.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
+import { useState, MouseEvent } from "react";
 import { Icon } from "..";
 import styles from "./input.module.scss";
 
 export const Input = (props: InputProps): JSX.Element => {
-  //TODO: fix click on clear button unfocusing input. Should capture unfocuse event and ignore it
   const [ searchText, setSearchText ] = useState<string>("");
   
   const clearSearch = () => setSearchText("");
@@ -19,7 +18,8 @@ export const Input = (props: InputProps): JSX.Element => {
         type="text"
         placeholder="Filter issues"
         onChange={e => setSearchText(e.target.value)}
-        className={styles.container}
+        onClick={() => console.log("CLICK INPUT")}
+        className={`${styles.container} ${searchText.length > 0 && styles.expanded}`}
       />
       <span className={styles.icon_container}>
         {renderIcon()}
@@ -35,7 +35,8 @@ const SearchIcon = (): JSX.Element => (
 )
 
 const ClearIcon = ({ onClick }: ClearIconProps): JSX.Element => (
-  <button onClick={onClick} className={`${styles.icon} ${styles.clear}`}>
+  //onMouseDown is needed because blur (unfocus) happens before 'click' event, but not before 'onMouseDown'
+  <button onMouseDown={onClick} className={`${styles.icon} ${styles.clear}`}>
     <Icon name="close" size={16} />
   </button>
 )
