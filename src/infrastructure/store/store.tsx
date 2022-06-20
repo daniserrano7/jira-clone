cea101272d64d1649f6cdbb8b4c6e537183fd831
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-06-20 20:56:47 +0200

    [UPD] refactor minor things

--- src/infrastructure/store/store.tsx
+++ src/infrastructure/store/store.tsx
@@ -1,10 +1,12 @@
 import { makeAutoObservable } from "mobx";
-import { Project, createProject, projectMock } from "domain/project";
+import { Project, projectMock } from "domain/project";
 import { Issue } from "domain/issue";
+import { User, userMock1 } from "domain/user";
 
 const store: Store = makeAutoObservable({
-  project: createProject(projectMock),
+  project: projectMock,
   editingIssue: null,
+  user: userMock1,
 });
 
 export const useStore = (): Store => store;
@@ -12,4 +14,5 @@ export const useStore = (): Store => store;
 interface Store {
   project: Project;
   editingIssue: Issue | null;
+  user: User;
 }
