6a72d48b2849a344a1ad3b189720d5b8baf13649
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-06-22 16:49:29 +0200

    [ADD] textarea autosize component

---
+++ src/ui/containers/project/board/issue-panel/textarea-autosize/textarea-autosize.container.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState, useRef } from "react";
+import styles from "./textarea-autosize.module.scss";
+
+export const TextareaAutosize = (props: TitleProps): JSX.Element => {
+  const { value, setValue, placeholder, onFocus, onBlur } = props;
+
+  const [ textareaHeight, setTextareaHeight ] = useState<number>(40);
+  const textareaRef = useRef<HTMLParagraphElement>(null);
+
+  const handleOnFocus = (e: React.FocusEvent<HTMLTextAreaElement>) => {
+    const target = e.currentTarget;
+    const length = target.value.length;
+    // Place cursor at the end of the current text
+    target.setSelectionRange(length, length);
+    if (onFocus) onFocus();
+  }
+
+  const handleTitleChange = (e: React.FormEvent<HTMLTextAreaElement>): void => {
+    const value = e.currentTarget.value;
+    setValue(value);
+  }
+
+  useEffect(() => {
+    if (!textareaRef.current) return;
+
+    setTextareaHeight(textareaRef.current.scrollHeight);
+  }, [value]);
+
+  return (
+    <div className={styles.container}>
+      <textarea
+        value={value}
+        onChange={handleTitleChange}
+        placeholder={placeholder}
+        onFocus={handleOnFocus}
+        onBlur={onBlur}
+        style={{ height: `${textareaHeight}px`}}
+        autoFocus
+      />
+      <p ref={textareaRef}>
+        {value || placeholder}
+      </p>
+    </div>
+  )
+}
+
+interface TitleProps {
+  value: string;
+  setValue: (value: string) => void;
+  placeholder: string;
+  onFocus?: () => void;
+  onBlur?: () => void;
+}
