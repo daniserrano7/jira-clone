b008c6d7d419793dd293d6e6c4cdbbab9976c683
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-06-22 16:50:12 +0200

    [UPD] use textarea autosize in title

--- src/ui/containers/project/board/issue-panel/title/title.container.tsx
+++ src/ui/containers/project/board/issue-panel/title/title.container.tsx
@@ -1,68 +1,43 @@
 import { useState } from "react";
+import { TextareaAutosize } from "../textarea-autosize";
 import styles from "./title.module.scss";
 
 export const Title = (): JSX.Element => {
   const [ title, setTitle ] = useState<string>("");
   const [ isFocus, setIsFocus ] = useState<boolean>(true);
 
-  const MAX_LENGTH = 60;
+  const MAX_LENGTH = 6000;
   const isMaxLength = title.length >= MAX_LENGTH;
+  const requireError = !isFocus && title.length === 0;
 
-  const startEditing = () => setIsFocus(true);
-  const stopEditing = () => setIsFocus(false);
+  const requireErrorStyles = requireError ? styles.require_error : undefined
+  const maxLengthStyles = isMaxLength ? styles.max_length : undefined
 
-  const onFocus = (e: React.FocusEvent<HTMLTextAreaElement>) => {
-    const target = e.currentTarget;
-    // Place cursor at the end of the current text
-    target.setSelectionRange(target.value.length, target.value.length);
-    startEditing();
-  }
-
-  const handleTitleChange = (e: React.FormEvent<HTMLTextAreaElement>): void => {
-    const value = e.currentTarget.value;
-
-    if (value.length <= MAX_LENGTH) {
-      setTitle(value);
-    }
-  }
-
-  const EditTitle = (): JSX.Element => (
-    <>
-      <textarea
-        value={title}
-        onChange={handleTitleChange}
-        placeholder="Write the title"
-        onFocus={onFocus}
-        onBlur={stopEditing}
-        rows={2}
-        className={styles.edit_title}
-        autoFocus
-      />
-      <span className={`
-        ${styles.counter}
-        ${isMaxLength ? styles.max_length : undefined}
-      `}>
-        {title.length} / {MAX_LENGTH}
-      </span>
-    </>
-  );
-
-  const ViewTitle = (): JSX.Element => (
-    <div
-      onClick={startEditing} 
-      className={styles.view_title}
-    >
-      {title}
-    </div>
-  )
+  const onFocus = () => setIsFocus(true);
+  const onBlur = () => setIsFocus(false);
 
   return (
-    <div className={styles.container}>
-      {isFocus
-        ? <EditTitle />
-        : <ViewTitle />
-      }
-      
+    <div className={`
+      ${styles.container}
+      ${requireErrorStyles}
+    `}>
+      <TextareaAutosize
+        value={title}
+        setValue={setTitle}
+        placeholder="Write the title"
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+      {requireError && (
+        <span className={styles.error_message}>
+          This field is required
+        </span>
+      )}
+      {isFocus && (
+        <span className={`${styles.counter} ${maxLengthStyles}`}>
+          {title.length} / {MAX_LENGTH}
+        </span>
+      )}
     </div>
   )
 }
