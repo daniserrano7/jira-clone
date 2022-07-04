1a23aca529490dc86f4811c1d3308f83e42e87b0
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-07-04 17:48:03 +0200

    [ADD] spinner at init app

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,31 +1,12 @@
-import { useState, useEffect } from "react";
 import { observer } from "mobx-react-lite";
-import db from "infrastructure/db";
-import { appStore, projectStore } from "infrastructure/store";
 import { Header } from "./containers/header";
 import { Project } from "./containers/project";
 
 const App = observer((): JSX.Element => {
-  const [ isLoaded, setIsLoaded ] = useState<boolean>(false);
-
-  useEffect(() => {
-    db.open();
-    db.on("ready", () => {
-      Promise.all([
-        appStore.fetchInitData(),
-        projectStore.fetchInitData()
-      ])
-        .then(() => setIsLoaded(true));
-    });
-  }, []);
-
   return (
     <div className="app">
       <Header />
-      {isLoaded
-        ? <Project />
-        : <div>LOADING</div>
-      }
+      <Project />
     </div>
   );
 });
