08e55aeb1c99951fe22b998d6fe287b7f750a288
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-07-05 16:51:31 +0200

    [FIX] vite fonts import

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./ui/app-entry";
 import "./ui/styles/index.scss";
+import "virtual:fonts.css";
 
 ReactDOM.render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 import eslint from "vite-plugin-eslint";
+import ViteFonts from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,46 @@ export default defineConfig({
     tsconfigPaths(),
     svgr(),
     eslint(),
+    ViteFonts({
+      custom: {
+        families: [
+          {
+            name: "CircularStdLight",
+            local: "CircularStdLight",
+            src: "./src/ui/assets/fonts/CircularStd-Book.woff2",
+          }, {
+            name: "CircularStdLight",
+            local: "CircularStdLight",
+            src: "./src/ui/assets/fonts/CircularStd-Book.woff",
+          }, {
+            name: "CircularStdMedium",
+            local: "CircularStdMedium",
+            src: "./src/ui/assets/fonts/CircularStd-Medium.woff2",
+          }, {
+            name: "CircularStdMedium",
+            local: "CircularStdMedium",
+            src: "./src/ui/assets/fonts/CircularStd-Medium.woff",
+          }, {
+            name: "CircularStdBold",
+            local: "CircularStdBold",
+            src: "./src/ui/assets/fonts/CircularStd-Bold.woff2",
+          }, {
+            name: "CircularStdBold",
+            local: "CircularStdBold",
+            src: "./src/ui/assets/fonts/CircularStd-Bold.woff",
+          }, {
+            name: "CircularStdBlack",
+            local: "CircularStdBlack",
+            src: "./src/ui/assets/fonts/CircularStd-Black.woff2",
+          }, {
+            name: "CircularStdBlack",
+            local: "CircularStdBlack",
+            src: "./src/ui/assets/fonts/CircularStd-Black.woff",
+          }
+        ],
+        display: "auto",
+        preload: true,
+      }
+    })
   ],
 });
