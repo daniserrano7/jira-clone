9ad9b69d7e47f9bfa318482e2f22a2a1134f490c
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-11-08 20:20:15 +0100

    update project component

--- src/ui/containers/project/project.tsx
+++ src/ui/containers/project/project.tsx
@@ -1,28 +1,32 @@
-import { useState, useEffect } from 'react';
-import db from 'infrastructure/db';
-import { appStore, projectStore } from 'infrastructure/store';
-import { Sidebar } from 'ui/containers/project/sidebar';
-import { Board } from './board';
-import styles from './project.module.scss';
+import { useState, useEffect } from "react";
+import db from "infrastructure/db";
+import { appStore, projectStore } from "infrastructure/store";
+import { Sidebar } from "ui/containers/project/sidebar";
+import { Board } from "./board";
 
 export const Project = (): JSX.Element => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     db.open();
-    db.on('ready', () => {
+    db.on("ready", () => {
       Promise.all([appStore.fetchInitData(), projectStore.fetchInitData()]).then(() =>
         setIsLoaded(true)
       );
     });
   }, []);
 
-  if (!isLoaded) return <LoadingScreen />;
+  if (!isLoaded)
+    return (
+      <div className="flex h-full w-full items-center justify-center">
+        <LoadingScreen />
+      </div>
+    );
 
   return (
-    <div className={styles.container}>
+    <div className="flex h-full flex-grow bg-white">
       <Sidebar />
-      <div className={styles.board_container}>
+      <div className="z-10 h-full w-full flex-grow py-6 px-5">
         <Board />
       </div>
     </div>
@@ -31,11 +35,9 @@ export const Project = (): JSX.Element => {
 
 const LoadingScreen = (): JSX.Element => {
   return (
-    <div className={styles.loading_screen}>
-      <div className={styles.loader}>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
+    <span className="relative flex flex-col items-center justify-center">
+      <span className="relative inline-flex h-12 w-12 animate-ping rounded-full bg-primary-main opacity-75"></span>
+      <span className="mt-6">Loading...</span>
+    </span>
   );
 };
