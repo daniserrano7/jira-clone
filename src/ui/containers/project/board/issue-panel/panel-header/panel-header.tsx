c4111b0074872e2a8ac4a537be68a6a93ccc5bb7
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-11-09 20:03:41 +0100

    update issue panel header

--- src/ui/containers/project/board/issue-panel/panel-header/panel-header.tsx
+++ src/ui/containers/project/board/issue-panel/panel-header/panel-header.tsx
@@ -1,20 +1,27 @@
-import * as Dialog from '@radix-ui/react-dialog';
-import * as AlertDialog from '@radix-ui/react-alert-dialog';
-import { IssueId } from 'domain/issue';
-import { Icon } from 'ui/components/icon';
-import styles from './panel-header.module.scss';
+import * as Dialog from "@radix-ui/react-dialog";
+import * as AlertDialog from "@radix-ui/react-alert-dialog";
+import cx from "classix";
+import { IssueId } from "domain/issue";
+import { Icon } from "ui/components/icon";
 
-export const PanelHeader = ({ id, onDeleteIssue, onClose }: PanelHeaderProps): JSX.Element => {
+export const PanelHeader = ({
+  id,
+  onDeleteIssue,
+  onClose,
+}: PanelHeaderProps): JSX.Element => {
   return (
-    <div className={styles.container}>
-      <span className={styles.issue_type}>
-        <span className={styles.icon}>
+    <div className="flex">
+      <span className="flex flex-grow items-center">
+        <span className="flex items-center">
           <Icon name="task" size={16} />
         </span>
-        <span className={styles.id}>{id}</span>
+        <span className="ml-1 text-font-light text-opacity-80">{id}</span>
       </span>
-      <CloseModalDialog action={onDeleteIssue} />
-      <Dialog.Close onClick={onClose} className={styles.header_button}>
+      <DeleteIssueModalDialog action={onDeleteIssue} />
+      <Dialog.Close
+        onClick={onClose}
+        className="ml-3 flex cursor-pointer rounded border-none p-1.5 text-icon hover:bg-grey-300"
+      >
         <Icon name="close" size={24} />
       </Dialog.Close>
     </div>
@@ -27,32 +34,45 @@ interface PanelHeaderProps {
   onClose: () => void;
 }
 
-const CloseModalDialog = ({ action }: CloseModalDialogProps): JSX.Element => {
+const DeleteIssueModalDialog = ({
+  action,
+}: CloseModalDialogProps): JSX.Element => {
+  const buttonBaseClass = cx(
+    "border-none py-1.5 px-3.5 rounded bg-grey-300 font-primary-bold cursor-pointer"
+  );
+
   return (
     <AlertDialog.Root>
-      <AlertDialog.Trigger
-        className={`
-        ${styles.header_button} 
-        ${styles.delete_button}
-      `}
-      >
+      <AlertDialog.Trigger className="flex cursor-pointer rounded border-none p-1.5 text-icon hover:bg-grey-300 hover:text-error-main">
         <Icon name="delete" size={24} />
       </AlertDialog.Trigger>
-      <AlertDialog.Portal className={styles.alert_portal}>
-        <AlertDialog.Content className={styles.content}>
-          <AlertDialog.Title className={styles.title}>Delete issue?</AlertDialog.Title>
-          <AlertDialog.Description className={styles.description}>
-            This action is permanent and cannot be undone. Are you sure you want to remove this
-            issue completely?
+      <AlertDialog.Portal>
+        <AlertDialog.Overlay className="fixed top-0 left-0 z-50 h-full w-full bg-black bg-opacity-40" />
+        <AlertDialog.Content className="fixed top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 rounded bg-white p-5 shadow-lg">
+          <AlertDialog.Title className="mb-5 font-primary-black text-3xl">
+            Delete issue?
+          </AlertDialog.Title>
+          <AlertDialog.Description className="font-primary-light leading-6 text-font-light">
+            This action is permanent and cannot be undone. Are you sure you want
+            to remove this issue completely?
           </AlertDialog.Description>
-          <div className={styles.buttons}>
-            <AlertDialog.Cancel className={styles.cancel}>Cancel</AlertDialog.Cancel>
-            <AlertDialog.Action onClick={action} className={styles.action}>
+          <div className="mt-8 flex w-full justify-end gap-4">
+            <AlertDialog.Cancel
+              className={cx("hover:bg-grey-400", buttonBaseClass)}
+            >
+              Cancel
+            </AlertDialog.Cancel>
+            <AlertDialog.Action
+              onClick={action}
+              className={cx(
+                "bg-error-light text-error-dark hover:bg-error-hover",
+                buttonBaseClass
+              )}
+            >
               Delete
             </AlertDialog.Action>
           </div>
         </AlertDialog.Content>
-        <AlertDialog.Overlay className={styles.overlay} />
       </AlertDialog.Portal>
     </AlertDialog.Root>
   );
