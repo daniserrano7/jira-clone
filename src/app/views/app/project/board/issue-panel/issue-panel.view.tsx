e3899db0a677581ca91b6b1f63f2dc265fbf9a30
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-12-02 17:56:08 +0100

    add pending ui to issue panel actions

--- src/app/views/app/project/board/issue-panel/issue-panel.view.tsx
+++ src/app/views/app/project/board/issue-panel/issue-panel.view.tsx
@@ -1,5 +1,10 @@
 import { useState, useRef } from "react";
-import { Form, useSubmit, useSearchParams } from "@remix-run/react";
+import {
+  Form,
+  useSubmit,
+  useSearchParams,
+  useTransition,
+} from "@remix-run/react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { userMock1 } from "@domain/user";
 import { Issue } from "@domain/issue";
@@ -15,7 +20,6 @@ import { SelectPriority } from "./select-priority";
 import { SelectAsignee } from "./select-asignee";
 import { useAppStore } from "@app/views/app/app.store";
 import { CategoryType } from "@domain/category";
-// import { textAreOnlySpaces } from "@app/utils";
 
 export const IssuePanel = ({ issue }: Props): JSX.Element => {
   const [comments, setComments] = useState<Comment[]>(issue?.comments || []);
@@ -23,6 +27,8 @@ export const IssuePanel = ({ issue }: Props): JSX.Element => {
   const formRef = useRef<HTMLFormElement>(null);
   const submit = useSubmit();
   const params = useSearchParams();
+  const transition = useTransition();
+  console.log("TRANSITION STATE: ", transition.state);
   const initStatus = (params[0].get("category") as CategoryType) || "TODO";
 
   const handleFormSumbit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -133,9 +139,17 @@ export const IssuePanel = ({ issue }: Props): JSX.Element => {
                 </span>
                 <button
                   type="submit"
-                  className="w-fit cursor-pointer justify-self-center rounded border-none bg-primary-main py-2 px-8 font-primary-bold text-lg text-white hover:bg-primary-main-hover"
+                  className="flex w-fit cursor-pointer items-center gap-4 justify-self-center rounded border-none bg-primary-main py-2 px-8 font-primary-bold text-lg text-white enabled:hover:bg-primary-main-hover disabled:cursor-not-allowed disabled:opacity-60"
+                  disabled={transition.state !== "idle"}
                 >
-                  Done
+                  {transition.state !== "idle" ? (
+                    <>
+                      Submmiting
+                      <Spinner />
+                    </>
+                  ) : (
+                    "Done"
+                  )}
                 </button>
               </div>
             </Form>
@@ -149,3 +163,22 @@ export const IssuePanel = ({ issue }: Props): JSX.Element => {
 interface Props {
   issue?: Issue;
 }
+
+const Spinner = (): JSX.Element => (
+  <svg
+    aria-hidden="true"
+    className="dark:text-gray-600 mr-2 inline-block h-5 w-5 animate-spin fill-grey-600"
+    viewBox="0 0 100 101"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+      fill="currentBaseColor"
+    />
+    <path
+      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+      fill="currentColor"
+    />
+  </svg>
+);
