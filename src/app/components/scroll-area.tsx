c6d0c1fd321d54742e894955a50f7070b716c55f
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-12-09 16:23:35 +0100

    update scroll bar dark theme

--- src/app/components/scroll-area.tsx
+++ src/app/components/scroll-area.tsx
@@ -17,7 +17,7 @@ export const ScrollArea = ({
     <ScrollAreaPrimitive.Scrollbar
       orientation="vertical"
       className={cx(
-        "flex w-2 touch-none select-none rounded-full bg-grey-400 duration-150 ease-out hover:bg-grey-600"
+        "flex w-2 touch-none select-none rounded-full bg-grey-400 duration-150 ease-out hover:bg-grey-600 dark:bg-dark-100"
       )}
     >
       <ScrollAreaPrimitive.Thumb
