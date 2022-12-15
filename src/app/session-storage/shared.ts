c2158ea6075521ba712a151211cdc70a7433d748
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-12-15 20:30:16 +0100

    fix env var

--- src/app/session-storage/shared.ts
+++ src/app/session-storage/shared.ts
@@ -1,4 +1,4 @@
-export const SESSION_SECRET = process.env.SESSION_SECRET;
+export const SESSION_SECRET = process.env.SESSION_SECRET || "default-secret";
 
 if (!SESSION_SECRET) {
   throw new Error("SESSION_SECRET must be set");
