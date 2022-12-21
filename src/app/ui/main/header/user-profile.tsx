da008906433b92b33bb4c7824608075f3d6c846a
Daniel Serrano <danielserrano.contacto@gmail.com>
2022-12-21 16:17:14 +0100

    handle form submit and menu closing

--- src/app/ui/main/header/user-profile.tsx
+++ src/app/ui/main/header/user-profile.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Form } from "@remix-run/react";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { FaPowerOff } from "react-icons/fa";
@@ -7,29 +6,20 @@ import { useUserStore } from "@app/store/user.store";
 import { UserAvatar } from "@app/components/user-avatar";
 
 export const UserProfile = (): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(false);
   const { user } = useUserStore();
 
-  const openMenu = () => setIsOpen(true);
-  const closeMenu = () => setIsOpen(false);
-
   return (
-    <DropdownMenu.Root open={isOpen}>
-      <DropdownMenu.Trigger
-        onClick={openMenu}
-        className="ml-1 rounded-full outline outline-2 outline-grey-300 hover:outline-primary-main dark:outline-grey-600 dark:hover:outline-white"
-      >
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger className="ml-1 rounded-full outline outline-2 outline-grey-300 hover:outline-primary-main dark:outline-grey-600 dark:hover:outline-white">
         <UserAvatar {...user} />
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content
-          onEscapeKeyDown={closeMenu}
-          onPointerDownOutside={closeMenu}
           align="end"
           sideOffset={5}
           className={cx(
             "z-50 rounded bg-white shadow-md-dark dark:bg-dark-400",
-            "radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down"
+            "radix-side-bottom:animate-slide-down radix-side-top:animate-slide-up"
           )}
         >
           <DropdownMenu.Item className="flex flex-col items-center p-3 !outline-none">
@@ -41,10 +31,11 @@ export const UserProfile = (): JSX.Element => {
             <Form action="action/logout" method="post">
               <button
                 type="submit"
+                onClick={(e) => e.stopPropagation()} // To prevent dropdown menu from closing
                 className="flex w-full items-center gap-2 rounded p-2 text-sm text-error-main hover:bg-error-light dark:text-error-main-dark dark:hover:bg-dark-100"
               >
                 <FaPowerOff />
-                <span className="">Log out</span>
+                <span>Log out</span>
               </button>
             </Form>
           </DropdownMenu.Item>
