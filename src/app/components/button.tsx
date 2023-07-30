f7e16a1815e2eefa9f6442a041b1958401a87041
Daniel Serrano <60034715+daniserrano7@users.noreply.github.com>
2023-07-30 21:43:29 +0200

    refactor: button neutral variant

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,10 +1,19 @@
 import cx from "classix";
 
-export const Button = ({ children, className, ...props }: Props) => {
+export const Button = ({
+  children,
+  variant = "primary",
+  className,
+  ...props
+}: Props) => {
   return (
     <button
       className={cx(
-        "rounded bg-background-brand p-2 text-font-inverse hover:bg-background-brand-hovered active:bg-background-brand-pressed",
+        "flex items-center justify-center rounded p-2",
+        variant === "primary" &&
+          "bg-background-brand text-font-inverse hover:bg-background-brand-hovered active:bg-background-brand-pressed",
+        variant === "neutral" &&
+          "bg-background-accent-grey-subtlest hover:bg-background-accent-grey-subtler active:bg-background-accent-grey-subtle",
         className
       )}
       {...props}
@@ -16,4 +25,5 @@ export const Button = ({ children, className, ...props }: Props) => {
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  variant?: "primary" | "neutral";
 }
