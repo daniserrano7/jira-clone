72d85f07bf0e883a68819b4cebd0027c55f78000
Daniel Serrano <60034715+daniserrano7@users.noreply.github.com>
2023-08-03 14:21:37 +0200

    feat: setup storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,6 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [
-    "@remix-run/eslint-config", 
-    "@remix-run/eslint-config/node",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
+  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node", "eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
   rules: {
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/consistent-type-imports": "off"
