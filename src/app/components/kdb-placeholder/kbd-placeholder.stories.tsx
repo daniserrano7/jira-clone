475a89e38cd6eff000924004e04a68e0266a9c3b
Daniel Serrano <60034715+daniserrano7@users.noreply.github.com>
2023-08-03 16:42:11 +0200

    feat: add kbd storybook

---
+++ src/app/components/kdb-placeholder/kbd-placeholder.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Kbd } from "./kbd-placeholder";
+
+const meta: Meta<typeof Kbd> = {
+  title: "Components/Kbd",
+  component: Kbd,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    children: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Kbd>;
+
+export const Default: Story = {
+  args: {
+    children: "Key",
+  },
+};
+
+export const Sentence: Story = {
+  render: (_) => (
+    <p className="text-xs text-font-subtlest">
+      Press <Kbd>Shift</Kbd> + <Kbd>S</Kbd> to save
+    </p>
+  ),
+};
