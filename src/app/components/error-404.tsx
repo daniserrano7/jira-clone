4584ae8fbec95dd18557ac7eb6f0e5d831679f9d
Daniel Serrano <60034715+daniserrano7@users.noreply.github.com>
2023-08-03 12:16:14 +0200

    feat: add toast messages to unavailable pages

--- src/app/components/error-404.tsx
+++ src/app/components/error-404.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 import { ErrorBase } from "./error-base";
 
 export const Error404 = ({ message, href }: Props) => {
+  useEffect(() => {
+    toast.warning("Try to go back to the previous page.");
+  }, []);
+
   return <ErrorBase variant="404" message={message} href={href} />;
 };
 
