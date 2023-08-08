ede65045c7d3482cdfa8f82387bef950fab42c8f
Daniel Serrano <60034715+daniserrano7@users.noreply.github.com>
2023-08-08 16:21:11 +0200

    Updates

---
+++ assets/error-404.stories-1100b30a.js
@@ -0,0 +1,15 @@
+import{j as E}from"./jsx-runtime-29545a09.js";import{E as k}from"./error-base-5b2b9d56.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";const o=({message:t="Error 404: Not Found",href:_})=>E(k,{variant:"404",message:t,href:_});try{o.displayName="Error404",o.__docgenInfo={description:"",displayName:"Error404",props:{message:{defaultValue:{value:"Error 404: Not Found"},description:"",name:"message",required:!1,type:{name:"string"}},href:{defaultValue:null,description:"",name:"href",required:!0,type:{name:"string"}}}}}catch{}const N={title:"Components/Error404",component:o,parameters:{layout:"centered"},argTypes:{message:{control:{type:"text"}},href:{control:{type:"text"}}}},e={},r={args:{message:"This is the error message"}},s={args:{href:"/link-to-safe-place"}},a={args:{message:"This is the error message",href:"/link-to-safe-place"}};var n,c,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var i,p,d;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    message: "This is the error message"
+  }
+}`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var g,l,u;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    href: "/link-to-safe-place"
+  }
+}`,...(u=(l=s.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var f,h,y;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    message: "This is the error message",
+    href: "/link-to-safe-place"
+  }
+}`,...(y=(h=a.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};const S=["Default","Message","Link","MessageLink"];export{e as Default,s as Link,r as Message,a as MessageLink,S as __namedExportsOrder,N as default};
+//# sourceMappingURL=error-404.stories-1100b30a.js.map
