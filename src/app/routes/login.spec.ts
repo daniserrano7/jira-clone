35230d88c9ca895bb11759cdf9ad68f241c29fd2
Daniel Serrano <60034715+daniserrano7@users.noreply.github.com>
2023-09-09 15:12:32 +0200

    feat: add login test

--- src/app/routes/login.spec.ts
+++ src/app/routes/login.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("/login");
-  await expect(page).toHaveTitle(/Jira clone/);
+  await expect(page).toHaveTitle(/Jira clone - Login/);
 });
 
 test("page renders", async ({ page }) => {
