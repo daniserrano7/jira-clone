6054f500f3e11a7900a67d11867dd18e8a56bd07
Dani <danielserrano.contacto@gmail.com>
2025-10-26 00:17:01 +0200

    fix: type error

--- src/app/stories/utils.tsx
+++ src/app/stories/utils.tsx
@@ -1,11 +1,10 @@
-import type { ReactRenderer } from "@storybook/react";
 import type { PartialStoryFn } from "@storybook/csf";
 import { unstable_createRemixStub as createRemixStub } from "@remix-run/testing";
 import { userMock1 } from "@domain/user";
 import { UserContextProvider } from "@app/store/user.store";
 import { ThemeProvider, Theme, Preference } from "@app/store/theme.store";
 
-type Story = PartialStoryFn<ReactRenderer, Record<string, never>>;
+type Story = PartialStoryFn<any, Record<string, never>>;
 
 export const withMainContext = (Story: Story): JSX.Element => {
   return (
